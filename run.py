563928b845f2953ce421ddf90fd8163f79140319
mljubaj <monika.ljubaj@gmail.com>
2022-04-11 10:05:32 +0000

    Function to end is made, percantage of the score is calculated

--- run.py
+++ run.py
@@ -2,10 +2,15 @@ print("Hello Player!")
 
 name = input("What is your name? ")
 
-print(f"Welcome to the Irish quiz, {name.capitalize()}! The quiz is made of two parts. First part are YES or NO questions. You will see the correct answers after you finish all questions.\n Second part of the quiz are questions with 3 possible answers. (A, B or C)\n The game is NOT case sensitive. :) When you are ready, please write 'yes'.\n Good luck {name.capitalize()}!")
+print(f"Welcome to the Irish quiz, {name.capitalize()}! The quiz is made of YES or NO questions. You will see the correct answers after you finish all questions.\n The game is NOT case sensitive. :) When you are ready, please write 'yes'.\n Good luck {name.capitalize()}!")
+
+def end():
+    print(f"Thank you for playing, {name.capitalize()}! Goodbye!")
+    quit()
 
 ready = input(f"Are you ready to play {name.capitalize()}? (YES/NO) ")
 if ready.upper() != "YES":
+    end()
     quit()
 else:
     print(f"Let's start, {name.capitalize()}!")
@@ -18,32 +23,22 @@ answer5 = input("The official currency of Republic of Ireland is pound. (YES/NO)
 correct_answers = [answer1, answer2, answer3]
 incorrect_answers = [answer4, answer5]
 
-score = 0
 
+
+score = 0
 for answer in correct_answers:
     if answer.upper() == "YES":
         print("Correct! :) ")
         score += 1
     else:
         print("Incorrect! :( ")
-
 for answer in incorrect_answers:
     if answer.upper() == "NO":
-         print("Correct! :) ")
-         score += 1
+        print("Correct! :) ")
+        score += 1
     else:
         print("Incorrect! :( ")
+print(f"{name.capitalize()} scored {score}/5 points.")
 
-print(f"{name.capitalize()} scored {score} out of 5 points.")
+end()
 
-go_ahead = input("Do you want to continue? (YES/NO) ")
-
-if go_ahead.upper() == "NO":
-    print(f"{name.capitalize()} solved {(score/10)*100} % of the quiz. ")
-    print(f"Goodbye {name.capitalize()}!")
-    quit()
-else:
-    print(f"Great {name.capitalize()}! Let's continue!")
-
-
-        
